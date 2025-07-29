af486751e668483329403dbe8f38d04f85023949
Leonilso <129322347+leonilso@users.noreply.github.com>
2025-07-29 14:41:39 -0300

    javascript

---
+++ index.js
@@ -0,0 +1,17 @@
+let dicionarioLinks = {
+    "pYux8zKyVX4": "https://www.youtube.com/embed/pYux8zKyVX4?si=soMljqxUo2PITZi9",
+    "sv9dDtYnE1g": "https://www.youtube.com/embed/sv9dDtYnE1g?si=ZLWzBqqL1DN3EZnC",
+    "WRlfwBof66s": "https://www.youtube.com/embed/WRlfwBof66s?si=WKkLBKaaYCKDcue1",
+    "4p7axLXXBGU": "https://www.youtube.com/embed/4p7axLXXBGU?si=rt0qPZzpEDOYHSkP",
+    "OFLMhFuArXQ": "https://www.youtube.com/embed/OFLMhFuArXQ?si=Jm4VmLTOguSqOTzD",
+    "8DhQG27oLPs": "https://www.youtube.com/embed/8DhQG27oLPs?si=ttcv9csMvWUkHPo_"
+}
+
+
+function mudarVideo(elemento){
+    let videoIframe = document.getElementById('iframe-video');
+    let srcAntigo = videoIframe.src;
+    let chave = Object.values(dicionarioLinks).includes(srcAntigo);
+    videoIframe.src = dicionarioLinks[elemento.dataset.info];
+    elemento.firstElementChild.src = `https://img.youtube.com/vi/${chave}/maxresdefault.jpg`
+}
